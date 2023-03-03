15a34c0af51ff2fa0c38dd4ccb5ec566bcb16b51
Sam Sahoo <api.tester.acc003@gmail.com>
2023-03-03 13:57:44 +0530

    created Abstract Classes and Interfaces Tutorials in the JavaProjects

--- JavaTutorials/src/main/java/Tutorial/Tut_AbstractClass/Car.java
+++ JavaTutorials/src/main/java/Tutorial/Tut_AbstractClass/Car.java
@@ -2,4 +2,10 @@ package Tutorial.Tut_AbstractClass;
 
 public class Car extends Vehicle {
 
+	@Override
+	public void drive() {
+		// TODO Auto-generated method stub
+		System.out.println("Driving a Car now");
+	}
+
 }
--- JavaTutorials/src/main/java/Tutorial/Tut_AbstractClass/Motorcycle.java
+++ JavaTutorials/src/main/java/Tutorial/Tut_AbstractClass/Motorcycle.java
@@ -2,4 +2,10 @@ package Tutorial.Tut_AbstractClass;
 
 public class Motorcycle extends Vehicle {
 
+	@Override
+	public void drive() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
